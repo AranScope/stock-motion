dbd8e80dde8711dc69850e9bbd3790ab12c524e0
Jack Wearden <jack@jackwearden.co.uk>
2015-11-29 11:23:19 +0000

    Main

--- src/com/aranscope/Main.java
+++ src/com/aranscope/Main.java
@@ -1,11 +1,37 @@
 package com.aranscope;
 
+import com.leapmotion.leap.Controller;
+
+import javax.swing.*;
+import java.util.Random;
+
 public class Main {
     /**
      * Program entry.
      * @param args
      */
     public static void main(String[] args) {
-        new C1API();
+        Controller controller = new Controller(); //LeapMotion controller object.
+        LeapPanel panel = new LeapPanel(controller, 1300,700);
+
+        JFrame frame = new JFrame("Stock Motion");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+
+        frame.pack();
+
+        Random ra = new Random();
+        while(true){
+            if(ra.nextDouble() > 0.95){ //Adding a new stock with probability 0.05 per tick.
+                Stock stock = new Stock(panel.getWidth(), panel.getHeight());
+                panel.addStock(stock);
+            }
+            try{
+                Thread.sleep(20);
+            }catch(Exception e){}
+            panel.update();
+        }
+
     }
 }
